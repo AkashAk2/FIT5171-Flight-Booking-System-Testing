80c842dc4e222018df5034c902c94f08838fff9f
abal0041 <abal0041@student.monash.edu>
2023-03-24 13:58:23 +1100

    Added 2 test cases for buyTicket()

--- FlightBookingSystem/src/test/java/TicketSystemTest.java
+++ FlightBookingSystem/src/test/java/TicketSystemTest.java
@@ -9,7 +9,6 @@ public class TicketSystemTest {
     @BeforeEach
     void setUp() {
         ticketSystem = new TicketSystem();
-        //testPassenger = new Passenger("John", "Wick", 38, "Male", "babayaga@gmail.com", "1234567890", "P1234567", "1234567812345678", 123);
     }
 
     /*
@@ -17,8 +16,29 @@ public class TicketSystemTest {
             We are assuming that there is a BuyTicketResult class that acts as the return type of buyTicket().
             It contains a Boolean variable and the ticket object.
             If the ticket purchase is valid, the bool variable is set to true and a ticket object is saved and stored.
-    */
+            We are assuming that there is a getter method called getPassenger() to return the passenger details.
+            We are assuming that the buyTicket() returns BuyTicketResult object
 
+        CLASS DEFINITION: BuyTicketResult
+
+            public class BuyTicketResult {
+                private boolean successfulPurchase;
+                private Ticket ticket;
+
+                public BuyTicketResult(boolean successfulPurchase, Ticket ticket) {
+                    this.successfulPurchase = successfulPurchase;
+                    this.ticket = ticket;
+                }
+
+                public boolean isPurchased() {
+                    return successfulPurchase;
+                }
+
+                public Ticket getTicket() {
+                    return ticket;
+                }
+            }
+    */
 
     /*
         Test Case 1:
@@ -31,7 +51,7 @@ public class TicketSystemTest {
         int ticketId = 1111;
         BuyTicketResult result = ticketSystem.buyTicket(ticketId);
         assertTrue(result.isPurchased());
-        assertEquals(testPassenger, result.getTicket().getPassenger());
+        assertEquals(ticketSystem.getPassenger(), result.getTicket().getPassenger());
     }
 
     /*
@@ -46,6 +66,34 @@ public class TicketSystemTest {
         BuyTicketResult result = ticketSystem.buyTicket(ticketId);
         assertFalse(result.isPurchased());
         assertNull(result.getTicket());
+    }
+
+    /*
+        Test Case 3:
+            This test case is to check the buyTicket() for an already purchased ticket.
+            If the Ticket is already purchased, the ticket object will not be created and returns NULL.
+     */
+    @Test
+    void testBuyTicketAlreadyPurchased() throws Exception {
+        // Assuming the ticket with id:3333 exists and already purchased
+        int ticketId = 3333;
+        BuyTicketResult result = ticketSystem.buyTicket(ticketId);
+        assertFalse(result.isPurchased());
+        assertNull(result.getTicket());
+    }
+
+    /*
+        Test Case 4:
+            This test case is to check the buyTicket() for available seats.
+            If the Ticket is valid but no seats are available the ticket object will be created without seat allocation.
+     */
+    @Test
+    void testBuyTicketNoSeatsAvailable() throws Exception {
+        // Assuming that there are no seats left but the ticket with id:4444 can be purchased by a passenger.
+        int ticketId = 4444;
+        BuyTicketResult result = ticketSystem.buyTicket(ticketId);
+        assertTrue(result.isPurchased());
+        assertNotNull(result.getTicket());
     }
 
 
