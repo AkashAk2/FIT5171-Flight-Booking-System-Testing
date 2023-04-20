4a012fcebe3f7b983e757523fbd61c6e1a9bb7dd
Ashiklal Memanaparambil Asokalal <amem0005@student.monash.edu>
2023-04-20 17:34:51 +1000

    testing git - tutorial activity

--- FlightBookingSystem/src/test/java/TicketSystemTest.java
+++ FlightBookingSystem/src/test/java/TicketSystemTest.java
@@ -129,8 +129,6 @@ public class TicketSystemTest {
         assertEquals(2, chosenTickets.size());
     }
 
-    // This is a test to check the gitlab repo update -Ashik
-
     /*
         Test Case 7:
             The purpose of this test case is to use chooseTicket() to find direct flights between two
