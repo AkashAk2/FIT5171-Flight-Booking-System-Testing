c3668369e0af7cce9ac6787622db9ef17eb9fb70
Ashiklal Memanaparambil Asokalal <amem0005@student.monash.edu>
2023-05-19 15:12:48 +1000

    merged passenger class

--- FlightBookingSystem/src/main/java/Passenger.java
+++ FlightBookingSystem/src/main/java/Passenger.java
@@ -1,3 +1,6 @@
+/**
+ * Represents a passenger with personal and contact information.
+ */
 public class Passenger extends Person
 {
     private String email;
@@ -6,11 +9,30 @@ public class Passenger extends Person
     private int securityCode;
     private String passport;
 
+    /**
+     * Default constructor.
+     */
     public Passenger(){}
 
-    public Passenger(String firstName, String secondName, int age, String gender,String email, String phoneNumber, String passport, String cardNumber,int securityCode)
+    /**
+     * Constructor with parameters.
+     *
+     * @param firstName    The first name of the passenger
+     * @param secondName   The second name of the passenger
+     * @param age          The age of the passenger
+     * @param gender       The gender of the passenger
+     * @param email        The email address of the passenger
+     * @param phoneNumber  The phone number of the passenger
+     * @param passport     The passport number of the passenger
+     * @param cardNumber   The card number used for booking
+     * @param securityCode The security code associated with the card
+     */
+    public Passenger(String firstName, String secondName, int age, String gender,String email, String phoneNumber,
+                     String passport, String cardNumber,int securityCode)
     {
         super(firstName, secondName, age, gender);
+        if (email == null || phoneNumber == null || passport == null || cardNumber == null || (securityCode > 0 && (securityCode < 100 || securityCode > 999))) {
+            throw new IllegalArgumentException("All fields are required");}
         this.securityCode=securityCode;
         this.cardNumber=cardNumber;
         this.passport=passport;
@@ -23,9 +45,15 @@ public class Passenger extends Person
     }
 
     public void setEmail(String email) {
+        // correct email pattern
+        boolean emailFollowsPattern = email.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+
+        if (!emailFollowsPattern) {
+            throw new IllegalArgumentException("Email is not in right format");
+        }
+
         this.email = email;
     }
-
     public String getFirstName() {
         return super.getFirstName();
     }
@@ -42,6 +70,11 @@ public class Passenger extends Person
         super.setFirstName(firstName);
     }
 
+    /**
+     * Returns the passport details.
+     *
+     * @return The passport details
+     */
     public String getPassport() {
         return passport;
     }
@@ -50,22 +83,47 @@ public class Passenger extends Person
         super.setGender(gender);
     }
 
+    /**
+     * Returns the phone number.
+     *
+     * @return The phone number
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Returns the security code of the credit card.
+     *
+     * @return The security code
+     */
     public int getSecurityCode() {
         return securityCode;
     }
 
+    /**
+     * Returns the card number.
+     *
+     * @return The card number
+     */
     public String getCardNumber() {
         return cardNumber;
     }
 
+    /**
+     * Sets the card number.
+     *
+     * @param cardNumber The passenger's card number to set
+     */
     public void setCardNumber(String cardNumber) {
         this.cardNumber = cardNumber;
     }
 
+    /**
+     * Sets the security code.
+     *
+     * @param securityCode The passenger's security code to set
+     */
     public void setSecurityCode(int securityCode) {
         this.securityCode = securityCode;
     }
@@ -75,7 +133,16 @@ public class Passenger extends Person
         super.setAge(age);
     }
 
+    /**
+     * Sets the passport number.
+     *
+     * @param passport The passenger's passport to set
+     */
     public void setPassport(String passport) {
+        // Passport less than 10 characters
+        if (!(passport.length() < 10)) {
+            throw new IllegalArgumentException("Passport is not in the right format");
+        }
         this.passport = passport;
     }
 
@@ -84,7 +151,19 @@ public class Passenger extends Person
         return super.getGender();
     }
 
+    /**
+     * Sets the phone number.
+     *
+     * @param phoneNumber The passenger's phone number to set
+     */
     public void setPhoneNumber(String phoneNumber) {
+        // pattern for Australian phone number
+        boolean phoneFollowsPattern = phoneNumber.matches("^(\\+?61|0) ?4 ?[0-9]{2}([ -]?[0-9]{3}){2}$");
+
+        if (!phoneFollowsPattern) {
+            throw new IllegalArgumentException("Phone must begin with +61 or 04 or 05 and followed by 8 digits");
+        }
+
         this.phoneNumber = phoneNumber;
     }
 
