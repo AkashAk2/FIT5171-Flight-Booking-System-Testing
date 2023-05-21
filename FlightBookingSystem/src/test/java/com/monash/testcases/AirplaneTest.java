42e5f8a89514fa5f90ae523f45d059b0f899327d
Ashiklal Memanaparambil Asokalal <amem0005@student.monash.edu>
2023-05-22 00:30:44 +1000

    added more test cases

--- FlightBookingSystem/src/test/java/com/monash/testcases/AirplaneTest.java
+++ FlightBookingSystem/src/test/java/com/monash/testcases/AirplaneTest.java
@@ -4,6 +4,7 @@ import com.monash.mainclasses.Airplane;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -103,5 +104,44 @@ class AirplaneTest {
 
         // Assert that the retrieved airplane is null
         Assertions.assertNull(retrievedAirplane);
+    }
+
+    @Test
+    public void testSetAirplaneID() {
+        Airplane airplane = new Airplane(1, "Boeing 737", 20, 200, 10);
+        airplane.setAirplaneID(2);
+        assertEquals(2, airplane.getAirplaneID());
+    }
+
+    @Test
+    public void testSetBusinessSitsNumber() {
+        Airplane airplane = new Airplane(1, "Boeing 737", 20, 200, 10);
+        airplane.setBusinessSitsNumber(30);
+        assertEquals(30, airplane.getBusinessSitsNumber());
+    }
+
+    @Test
+    public void testSetEconomySitsNumber() {
+        Airplane airplane = new Airplane(1, "Boeing 737", 20, 200, 10);
+        airplane.setEconomySitsNumber(250);
+        assertEquals(250, airplane.getEconomySitsNumber());
+    }
+
+    @Test
+    public void testSetCrewSitsNumber() {
+        Airplane airplane = new Airplane(1, "Boeing 737", 20, 200, 10);
+        airplane.setCrewSitsNumber(15);
+        assertEquals(15, airplane.getCrewSitsNumber());
+    }
+
+    @Test
+    public void testSetInvalidSeatNumber() {
+        Airplane airplane = new Airplane(1, "Boeing 737", 20, 200, 10);
+        assertThrows(IllegalArgumentException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                airplane.setEconomySitsNumber(350);
+            }
+        });
     }
 }
--- FlightBookingSystem/src/test/java/com/monash/testcases/TicketTest.java
+++ FlightBookingSystem/src/test/java/com/monash/testcases/TicketTest.java
@@ -12,26 +12,18 @@ import static org.mockito.Mockito.when;
 
 // This class is responsible for testing the com.monash.fit5171.Ticket class.
 class TicketTest {
-    private Flight mockFlight;
-    private Passenger mockPassenger;
+    private Flight flight;
+    private Passenger passenger;
     private Ticket ticket;
 
     // Set up method to run before each test case.
     @BeforeEach
     void setUp() {
-        mockFlight = Mockito.mock(Flight.class);
-        mockPassenger = Mockito.mock(Passenger.class);
-        when(mockPassenger.getAge()).thenReturn(30);
+        flight = Mockito.mock(Flight.class);
+        passenger = Mockito.mock(Passenger.class);
+        when(passenger.getAge()).thenReturn(30);
 
-        this.ticket = new Ticket(1, 1000, mockFlight, true, mockPassenger);
-    }
-
-    // Test case for checking the ticket status.
-    @Test
-    void testTicketStatus() {
-        assertFalse(this.ticket.getTicketStatus());
-        this.ticket.setTicketStatus(true);
-        assertTrue(this.ticket.getTicketStatus());
+        this.ticket = new Ticket(1, 1000, flight, true, passenger);
     }
 
     // Test case for toggling the ticket status between true and false.
@@ -43,12 +35,6 @@ class TicketTest {
         this.ticket.setTicketStatus(false);
         assertFalse(this.ticket.getTicketStatus());
     }
-
-    // Test case for checking the ticket discount based on passenger's age.
-    @Test
-    void testTicketDiscount() {
-    }
-
 
     // Test case for checking the ticket price.
     @Test
@@ -66,35 +52,48 @@ class TicketTest {
         assertTrue(actualMessage.contains(expectedMessage));
     }
 
-    // Test case for checking ticket price and service tax.
-    @Test
-    void testTicketPriceAndServiceTax() {
-    }
-
     // Test case for checking if service tax is appliedd
     @Test
     void testServiceTaxApplied() {
         this.ticket.setPrice(1000);
         assertEquals(1120, this.ticket.getPrice());
     }
-    // Test case for checking the ticket service tax calculation.
-    @Test
-    void testTicketServiceTax() {
-    }
-
     // Test case for checking if a ticket has a valid flight and passenger.
     @Test
     void testTicketValidFlightAndPassenger() {
-        assertEquals(mockFlight, this.ticket.getFlight());
-        assertEquals(mockPassenger, this.ticket.getPassenger());
+        assertEquals(flight, this.ticket.getFlight());
+        assertEquals(passenger, this.ticket.getPassenger());
     }
 
     @Test
         // Test case for checking the ticket ID.
     void testTicketID() {
-        Ticket ticket = new Ticket(7890, 1000, new Flight(), false, mockPassenger);
+        Ticket ticket = new Ticket(7890, 1000, new Flight(), false, passenger);
         int expectedId = 7890;
         int actualId = ticket.ticketId();
         assertEquals(expectedId, actualId);
+    }
+
+    @Test
+    void testTicketStatus() {
+        Ticket ticket = new Ticket(1, 1000, flight, false, passenger);
+        assertFalse(ticket.ticketStatus());
+        ticket.setTicketStatus(true);
+        assertTrue(ticket.ticketStatus());
+    }
+
+    @Test
+    void testValidPriceAndServiceTax() {
+        Mockito.when(passenger.getAge()).thenReturn(30);
+        Ticket ticket = new Ticket(4, 1000, flight, false, passenger);
+        assertTrue(ticket.getPrice() >= 0);
+        assertTrue(ticket.getPrice() % 1 == 0); // Check if the price is an integer
+    }
+
+    @Test
+    void testValidFlightAndPassengerInfo() {
+        Ticket ticket = new Ticket(6, 1000, flight, false, passenger);
+        assertNotNull(ticket.getFlight());
+        assertNotNull(ticket.getPassenger());
     }
 }
